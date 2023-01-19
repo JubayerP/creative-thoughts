da8ae8a6c099ec4d97397b9c2b5b258015233b10
JubayerDev <ajashes858@gmail.com>
2023-01-20 02:00:56 +0600

    login with google system

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,8 +1,39 @@
+import { FcGoogle } from 'react-icons/fc'
+import { BsFacebook } from 'react-icons/bs'
+import { GoogleAuthProvider, FacebookAuthProvider, signInWithPopup } from 'firebase/auth';
+import { auth } from '../../firebase/firebase.config'
+import { useRouter } from 'next/router';
 
 const login = () => {
+    const route = useRouter()
+
+    // sign in with google
+    const googleProvider = new GoogleAuthProvider();
+    const facebookProvider = new FacebookAuthProvider();
+
+    const GoogleLogin = async () => {
+        try {
+            const result = await signInWithPopup(auth, googleProvider);
+            route.push('/')
+            // 38 mins
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
+
     return (
-        <div>
-            
+        <div className="shadow-xl mt-32 p-10 text-gray-700 rounded-lg">
+            <h2 className="text-2xl font-medium">Login Today</h2>
+
+            <div className="py-4">
+                <h3 className="py-4">Sign in with</h3>
+                <button onClick={GoogleLogin} className="text-white bg-gray-700 w-full font-medium rounded-lg flex align-middle p-4 gap-2 mb-6">
+                    <FcGoogle className='text-2xl'/>
+                    Sign in with Google</button>
+                <button className="text-white bg-indigo-400 w-full font-medium rounded-lg flex align-middle p-4 gap-2"><BsFacebook className='text-2xl'/> Sign in with Facebook</button>
+            </div>
         </div>
     );
 };
